8edb683fa06cf354465102971e819e427c617324
SingleArtist <95147229+SingleArtist@users.noreply.github.com>
2021-12-29 20:15:13 +0300

    Add files via upload

---
+++ MPP3/Mutex.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System;
+
+
+//Задание 3
+/*Реализовать консольную программу на языке C#, которая:
+- принимает в параметре командной строки путь к сборке .NET
+(EXE- или DLL-файлу);
+-загружает указанную сборку в память;
+-выводит на экран полные имена всех public -типов данных этой
+сборки, упорядоченные по пространству имен (namespace) и по
+имени.*/
+
+namespace ThirdTask
+{
+    class Mutex
+    {
+        private int  currentValue;
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref currentValue, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Unlock()
+        {
+            Interlocked.CompareExchange(ref currentValue, 0, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
